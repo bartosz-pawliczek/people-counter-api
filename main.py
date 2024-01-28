1b211193514cb58dfae51ae9bbcb988fe9c944a3
Bartosz Pawliczek <pawliczek.bartosz@gmail.com>
2024-01-28 16:13:49 +0100

    modify main.py file

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-from flask import Flask
+from flask import Flask, request
 from flask_restful import Resource, Api
 import cv2
+import numpy as np
+import requests
 
 app = Flask(__name__)
 api = Api(app)
@@ -9,20 +11,35 @@ hog = cv2.HOGDescriptor()
 hog.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
 
 
+def count_people(image):
+    boxes, weights = hog.detectMultiScale(image, winStride=(4, 4))
+    return len(boxes)
+
+
 class PeopleCounter(Resource):
     def get(self):
         img = cv2.imread('images/group_photo.jpg')
-        boxes, weights = hog.detectMultiScale(img, winStride=(4, 4))
-        return {'count': len(boxes)}
+        count = count_people(img)
+        return {'count': count}
 
 
-class HelloWorld(Resource):
+class PeopleCounterLink(Resource):
     def get(self):
-        return {'hello': 'world'}
+        url = request.args.get('url')
+        if url:
+            response = requests.get(url)
+            if response.status_code == 200:
+                nparr = np.frombuffer(response.content, np.uint8)
+                img = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
+                count = count_people(img)
+                return {'count': count}
+            else:
+                return {'error': 'Image could not be retrieved'}, 400
+        return {'error': 'No URL provided'}, 400
 
 
 api.add_resource(PeopleCounter, '/')
-api.add_resource(HelloWorld, '/test')
+api.add_resource(PeopleCounterLink, '/link')
 
 if __name__ == '__main__':
     app.run(debug=True)
